4f743a870d84c91d0efee68a60e7e2f3999464b9
Liam <laxon26@gmail.com>
2021-11-17 11:21:38 -0500

    Added camera

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        extendDefaults: true,
+        types: {
+          "{}": false,
+        },
+      },
+    ],
+  },
+  settings: {
+    // here I can put path alias things, if ESLint starts caring about them
+    // see https://cheesecakelabs.com/blog/efficient-way-structure-react-native-projects/
+  },
+};
